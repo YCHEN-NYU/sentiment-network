98895b59c7937aa44b28a487a7ab2f15a54a5cdc
Yizhang Chen <yc1224@nyu.edu>
2017-07-20 18:16:56 -0400

    add .py for the class

--- classSentimentNetwork.py
+++ classSentimentNetwork.py
@@ -28,63 +28,44 @@ class SentimentNetwork:
         self.label_vocab = list(label_vocab)
         self.label_vocab_size = len(self.label_vocab)
         self.label2index = {label:i for i, label in enumerate(self.label_vocab)}
-         
         
     def init_network(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
-        # Store the number of nodes in input, hidden, and output layers.
         self.input_nodes = input_nodes
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
-
-        # Store the learning rate
         self.learning_rate = learning_rate
 
-        # Initialize weights
-        
-        # TODO: initialize self.weights_0_1 as a matrix of zeros. These are the weights between
-        #       the input layer and the hidden layer.
+        # initialize self.weights_0_1 as a matrix of zeros.(weights input->hidden layer)
         self.weights_0_1 = np.zeros((self.input_nodes, self.hidden_nodes), dtype=float)
-        
-        # TODO: initialize self.weights_1_2 as a matrix of random values. 
-        #       These are the weights between the hidden layer and the output layer.
+        print('self.weights_0_1 = ', self.weights_0_1)
+        # initialize self.weights_1_2 as a matrix of zeros.(weights hidden->output layer)
         self.weights_1_2 = np.random.normal(0.0, self.hidden_nodes**(-0.5), (self.hidden_nodes, self.output_nodes))
-        
-        # TODO: Create the input layer, a two-dimensional matrix with shape 
-        #       1 x input_nodes, with all values initialized to zero
+        # input layer layer_0, a two-dimensional matrix with shape (1, input_nodes), with all values initialized to zero
         self.layer_0 = np.zeros((1,self.input_nodes), dtype=float)
-    
         
     def update_input_layer(self,review):
-        # TODO: You can copy most of the code you wrote for update_input_layer 
-        #       earlier in this notebook. 
-        #
         #       However, MAKE SURE YOU CHANGE ALL VARIABLES TO REFERENCE
         #       THE VERSIONS STORED IN THIS OBJECT, NOT THE GLOBAL OBJECTS.
         #       For example, replace "layer_0 *= 0" with "self.layer_0 *= 0"
-        global self.layer_0
+        global layer_0
         # clear out previous state by resetting the layer to be all 0s
         self.layer_0 *= 0
     
         # TODO: count how many times each word is used in the given review and store the results in layer_0 
-        for word in review.split(" "):
+        # review here is a single block from the reviews
+        for word in review.split(' '):
             self.layer_0[0][word2index[word]] += 1
                 
     def get_target_for_label(self,label):
-        # TODO: Copy the code you wrote for get_target_for_label 
-        #       earlier in this notebook. 
         if (label == "POSITIVE"):
             return 1
         else:
             return 0
         
     def sigmoid(self,x):
-        # TODO: Return the result of calculating the sigmoid activation function
-        #       shown in the lectures
         return (lambda x: 1.0/(1.0 + np.exp(-x)))
     
     def sigmoid_output_2_derivative(self,output):
-        # TODO: Return the derivative of the sigmoid activation function, 
-        #       where "output" is the original output from the sigmoid fucntion 
         return sigmoid(output)*(1-sigmoid(output))
 
     def train(self, training_reviews, training_labels):
@@ -101,46 +82,33 @@ class SentimentNetwork:
         # loop through all the given reviews and run a forward and backward pass,
         # updating weights for every item
         for i in range(len(training_reviews)):
-            
-            
             # TODO: Get the next review and its correct label
             pre_process_data(training_reviews[i], training_labels[i])
+
             # TODO: Implement the forward pass through the network. 
-            #       That means use the given review to update the input layer, 
-            #       then calculate values for the hidden layer,
-            #       and finally calculate the output layer.
-            # 
-            #       Do not use an activation function for the hidden layer,
-            #       but use the sigmoid activation function for the output layer.
-            
-            hidden_inputs = np.dot(self.layer_0[i],  self.weights_0_1)
+            hidden_inputs = np.dot(layer_0,  self.weights_0_1)
             hidden_outputs = hidden_inputs
             final_inputs = np.dot(hidden_outputs, self.weights_1_2)
             final_outputs = final_inputs
             
             # TODO: Implement the back propagation pass here. 
-            #       That means calculate the error for the forward pass's prediction
-            #       and update the weights in the network according to their
-            #       contributions toward the error, as calculated via the
-            #       gradient descent and back propagation algorithms you 
-            #       learned in class.
             output_errors = training_labels[i] - final_outputs
             hidden_errors = (np.dot(self.weights_1_2, output_errors)).T
             
             output_error_term = output_errors*1.0
-            hidden_error_term = hidden_errors*hidden_outputs*(1-hidden_outputs)
-            
+            hidden_error_term = hidden_errors*1.0
+            #hidden_error_term = hidden_errors*hidden_outputs*(1-hidden_outputs)
             
             self.weights_1_2 += self.learning_rate*hidden_outputs[:, None]*output_error_term/1.0
-            self.weights_0_1 += self.learning_rate*self.layer_0[i][:, None]*hidden_error_term/1.0
+            self.weights_0_1 += self.learning_rate*self.layer_0[:, None]*hidden_error_term/1.0
             
             # TODO: Keep track of correct predictions. To determine if the prediction was
             #       correct, check that the absolute value of the output error 
             #       is less than 0.5. If so, add one to the correct_so_far count.
-            if(output_errors < 0.5):
+            if(abs(output_errors) < 0.5):
                 correct_so_far += 1
             else:
-                print('output_errors > 0.5')
+                print('abs(output_errors) > 0.5')
             # For debug purposes, print out our prediction accuracy and speed 
             # throughout the training process. 
 
@@ -196,11 +164,16 @@ class SentimentNetwork:
         #       Note: The review passed into this function for prediction 
         #             might come from anywhere, so you should convert it 
         #             to lower case prior to using it.
-        pre_process_data(review.lower())
-        hidden_inputs = np.dot(self.layer_0, self.weight_0_1)
-        hidden_outputs = sigmoid(hidden_inputs)
-        final_inputs = np.dot(hidden_outputs, self.weight_1_2)
+        #pre_process_data(review.lower())
+        hidden_inputs = np.dot(self.layer_0, self.weights_0_1)
+        hidden_outputs = hidden_inputs
+        final_inputs = np.dot(hidden_outputs, self.weights_1_2)
         final_outputs = final_inputs
+
         # TODO: The output layer should now contain a prediction. 
         #       Return `POSITIVE` for predictions greater-than-or-equal-to `0.5`, 
         #       and `NEGATIVE` otherwise.
+        if(final_outputs > 0.5):
+            return "POSITIVE"
+        else:
+            return "NEGATIVE"
